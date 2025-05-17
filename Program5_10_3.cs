7da50576da883329abc74937b3c7b45be87f1880
Room <70736853+8305040@users.noreply.github.com>
2025-05-17 16:02:57 +0800

    Add files via upload

---
+++ Program5_10_3.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using HW5_10_3;
+
+namespace HWS_10_3
+{
+    class Program
+    {
+        static void Main()
+        {
+            Car myCar = new Car("Toyota", 800000);
+            Console.WriteLine("車名: " + myCar.GetName());
+            Console.WriteLine("價格: " + myCar.GetPrice());
+        }
+    }
+}
---
+++ Program5_12_3.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using HW5_12_3;
+
+namespace UnitConverter
+{
+    public delegate double ConvertToInches(double value);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("請輸入數值：");
+            double input = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("請選擇轉換方式：");
+            Console.WriteLine("1. 英尺轉英吋");
+            Console.WriteLine("2. 英碼轉英吋");
+            Console.Write("請輸入選項（1 或 2）：");
+            string choice = Console.ReadLine();
+
+       
+            ConvertToInches converter = null;
+
+            if (choice == "1")
+            {
+                FeetConverter feet = new FeetConverter();
+                converter = feet.FeetToInches;
+            }
+            else if (choice == "2")
+            {
+                YardConverter yard = new YardConverter();
+                converter = yard.YardToInches;
+            }
+            else
+            {
+                Console.WriteLine("無效的選項！");
+                return;
+            }
+
+            
+            double result = converter(input);
+            Console.WriteLine("轉換結果為：" + result + " 英吋");
+        }
+    }
+}
